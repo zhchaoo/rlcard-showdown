e6c5b1dcb6086cf6426fe9b7f2179554277f76bc
zhouchao <zc98770@alibaba-inc.com>
2021-10-11 10:32:19 +0800

    revert ip

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://30.210.212.1:8000';
-const douzeroDemoUrl = 'http://30.210.212.1:5000';
+const apiUrl = 'http://127.0.0.1:8000';
+const douzeroDemoUrl = 'http://127.0.0.1:5000';
 
 export { apiUrl, douzeroDemoUrl };
